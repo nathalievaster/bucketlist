4ac8cd8349324f4c8556c187ea53c3d2608cdfe5
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-11-20 17:14:00 +0100

    uppdaterade tabell för uppgiften

--- ai.php
+++ ai.php
@@ -1,22 +1,11 @@
-<?php include ("includes/config.php"); ?>
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <header>
-        <a href="index.php">Bucketlist</a>
-        <nav>
-            <ul>
-                <li><a href="index.php">Startsida</a></li>
-                <li><a href="bucketlist.php">Bucketlist</a></li>
-                <li><a href="addbucketlist.php">Lägg till i bucketlist</a></li>
-                <li><a href="ai.php">AI-verktyg jag använt</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main></main>
+<?php
+    $title = "Startsida";
+    include 'includes/header.php';
+?>
+
+    <h1>Välkommen till min PHP-sida!</h1>
+    <p>Denna sidan ska användas som bas i kursens gång.</p>
+
+<?php
+    include 'includes/footer.php';
+?>
--- install.php
+++ install.php
@@ -11,16 +11,12 @@ $sql = "
 DROP TABLE IF EXISTS bucketlist;
 CREATE TABLE bucketlist(
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(6) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    progression VARCHAR(2) NOT NULL,
-    syllabus varchar(255) NOT NULL
+    description TEXT NOT NULL,
+    priority TINYINT NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 ";
-
-
-$sql .= "INSERT INTO courses(code, name, progression, syllabus)VALUES('AK001A', 'Teori och metod', 'AV', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/AK001A');";
-$sql .= "INSERT INTO courses(code, name, progression, syllabus)VALUES('DT084G', 'Introduktion till programmering med JavaScript', 'A', 'https://www.miun.se/utbildning/kursplaner-och-utbildningsplaner/DT');";
 
 // Skicka SQL-frågan till servern
 if($db->multi_query($sql)) {
