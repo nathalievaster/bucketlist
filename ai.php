ff383b9279ddc1612dbfa448b2bc470d3a22ac64
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-11-23 14:59:34 +0100

    innehåll på startsida och AI

--- ai.php
+++ ai.php
@@ -2,14 +2,29 @@
     $title = "Startsida";
     include 'includes/header.php';
 ?>
-
-    <h1>Välkommen till min PHP-sida!</h1>
-    <p>Denna sidan ska användas som bas i kursens gång.</p>
-    <p>Jag använde chat-gpt för att fråga hur jag skulle göra med prioritet.
-        Fick förslaget med tinyint och det har jag använt mig av. Omvandlar sedan till
-        text i frontenden.
+<div class="text-container">
+    <h1>Användning av AI</h1>
+    <p>
+        Att använda AI för att generera ett helt projekt var absolut inget för mig
+        - jag klarade inte ens av att låta AI generera hela filer åt mig. Jag testade
+        använda agent-läget som finns inbyggt i Visual Studio Code, men tyckte koden
+        snabbt blev onödigt krånglig och svår att sätta sig in i. Jag utgick istället 
+        utifrån Mattias föreläsningar, och använde de auto-genererade kodförslagen som 
+        dyker upp medan man skriver. Många gånger raderade jag även dessa. Det är dock
+        till otroligt stor hjälp att få hjälp med kod som är ganska upprepande, som t.ex
+        mina setters och getters. När jag väl kodat hur jag ville att första skulle se ut,
+        så var det inga problem att använda AI-förslagen som dök upp på de andra (jag hade
+        ju på sätt och vis gjort klart mallen för hur jag ville att de skulle se ut.)
     </p>
-
+    <p>
+        Att använda Chat-gpt är något jag blivit van med under min tid på Webbutvecklings-programmet,
+        och i vissa fall har det varit ovärdeligt. Men inte heller där gillade jag att använda
+        helt färdig-genererad kod, utan mer som ett bollplank. Jag gillar att bolla idéer, få tips
+        på hur man bäst lägger upp struktur, och kan ibland bara vilja stämma av "har jag förstått 
+        detta rätt?". I detta projekt frågade jag Chat-GPT hur jag bäst skulle göra med prioritet, och 
+        fick då förslaget med att använda tinyint och sedan omvandla detta till text i frontenden.
+    </p>
+</div>
 <?php
     include 'includes/footer.php';
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php
-    $title = "Startsida";
-    include 'includes/header.php';
+$title = "Startsida";
+include 'includes/header.php';
 ?>
+<div class="text-container">
+    <h1>Moment 2</h1>
+    <h3>Om uppgiften</h3>
+    <p>
+        Den här webbplatsen är skapad som en del av kursmomentet där målet är att öva på PHP, databaskoppling och
+        objektorienterad programmering. Uppgiften går ut på att bygga en enklare applikation där man kan lägga till,
+        visa och hantera sin egen bucketlist. Projektet innehåller både ett ER-diagram för databasen och ett UML-diagram
+        för klasserna som används i lösningen.
+    </p>
+    <h3>Mina reflektioner</h3>
+    <p>
+        Det har varit en kul och ändå relativ lätt uppgift att laborera med för att sätta sig in i PHP.
+        Det underlättar givetvis att vi har lärt oss 2 programmeringsspråk innan detta, och att det nu 
+        börjar bli något lättare att förstå logiken i nya. Jag upplever ändå att detta är ett lätt och
+        roligt språk! När vi började testa använda ramverk som Angular där man delar upp koden mer och 
+        mer, började utvecklandet också kännas roligare och roligare. Det är skönt att snabbt kunna 
+        överblicka sina filer utan att behöva sitta och skrolla för att hitta olika fel. Att göra kopplingar
+        till databaser kändes också lättare än det har gjort i tidigare kurser!
+    </p>
 
-    <h1>Välkommen till min PHP-sida!</h1>
-    <p>Denna sidan ska användas som bas i kursens gång.</p>
+</div>
 
 <?php
-    include 'includes/footer.php';
+include 'includes/footer.php';
 ?>
